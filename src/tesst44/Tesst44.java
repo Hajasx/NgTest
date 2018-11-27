8b7532fa09638056d2b065cc105bdbb80287113b
László Molnár <laszlomolnar00@gmail.com>
2018-11-27 01:34:46 +0100

    teszt2

--- src/tesst44/Tesst44.java
+++ src/tesst44/Tesst44.java
@@ -15,6 +15,8 @@ public class Tesst44 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+         System.out.println("Hello, World");
+        
         // TODO code application logic here
     }
     
